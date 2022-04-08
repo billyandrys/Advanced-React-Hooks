094e2d01764e9a04d2462fbc659dce7457badb80
billyandryus <billyandrys@gmail.com>
2022-04-08 15:21:16 -0500

    cyclo life hook useState

--- src/App.js
+++ src/App.js
@@ -1,48 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
-class Button extends React.Component {
-  constructor(){
-    super()
-    this.state = {
-        counter:0,
-        counter2:2
-    } 
- }
-
- render() {
-   return <h1 onClick={()=>{
-     this.setState({
-       counter:this.state.counter + 1
-     })
-   }}>{this.state.counter} { this.state.counter2} </h1>
- }
+function Child() {
+  useEffect(()=>{
+    console.log('\tChild update')
+    return ()=>{
+      console.log('\tchild Cleanup called')
+    }
+  })
+  return <h1> I am Child </h1>
 }
 
 function App() {
-  
-  const [state, ssetState] = useState({
-    counter:0,
-    counter2:10
+  console.log('-------------> App render stared - incial --------------------')
+  const [count, setCount] = useState(()=>{
+    console.log('> Lazy init I called llamda 1')
+    return 0
   })
+  const [show, setShow] = useState(()=>{
+    console.log('> Lazy init 2 called llamada 2')
+    return false
+  })
+  useEffect(()=>{
+    console.log('> Counter updated se actualiza contador')
+    if(count === 5) {
+      setShow(true)
+    }
+    return ()=>{ console.log('> Cleaning up effect of counter')}
+  }, [count])
 
-  function setState(newState) {
-      ssetState(oldState => {
-        return {
-          ...oldState,
-          ...newState
-        }
-      })
-  }
+const AppComponent = <div>
+  <h1 onClick={()=>setCount((count)=> count + 1)}>
+    Counter 1: {count}
+  </h1>
+  <br/>
+  {show && <Child/>}
+</div>  
 
-  return (
-    <div className="App">
-      <h1 onClick={()=>{
-        setState({
-          counter: state.counter + 1 
-          })
-      }}> {state.counter} {state.counter2} </h1>     
-    </div>
-  );
+  console.log('> App render ended')
+  return AppComponent
 }
 
 export default App;
