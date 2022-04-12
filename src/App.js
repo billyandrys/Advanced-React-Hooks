0d4e250907a8e2fd6cf45c850ff8514ce9cb483c
billyandryus <billyandrys@gmail.com>
2022-04-12 11:38:05 -0500

    useRef

--- src/App.js
+++ src/App.js
@@ -1,43 +1,26 @@
-import React, { useState, useEffect } from 'react'
+import React, {  useRef, useEffect, useState } from 'react'
 
-function Child() {
-  useEffect(()=>{
-    console.log('\tChild update')
-    return ()=>{
-      console.log('\tchild Cleanup called')
-    }
-  })
-  return <h1> I am Child </h1>
-}
 
 function App() {
-  console.log('-------------> App render stared - incial --------------------')
-  const [count, setCount] = useState(()=>{
-    console.log('> Lazy init I called llamda 1')
-    return 0
-  })
-  const [show, setShow] = useState(()=>{
-    console.log('> Lazy init 2 called llamada 2')
-    return false
-  })
+  const obj = useRef(100)
+  const [count, setCount] = useState(0)
   useEffect(()=>{
-    console.log('> Counter updated se actualiza contador')
-    if(count === 5) {
-      setShow(true)
-    }
-    return ()=>{ console.log('> Cleaning up effect of counter')}
-  }, [count])
+      console.log('Effect')
+  },[obj])
 
-const AppComponent = <div>
-  <h1 onClick={()=>setCount((count)=> count + 1)}>
-    Counter 1: {count}
-  </h1>
-  <br/>
-  {show && <Child/>}
-</div>  
-
-  console.log('> App render ended')
-  return AppComponent
+  useEffect(()=>{
+    console.log('Effect ', obj)
+},[count])
+  
+  console.log('ref object ', obj)
+  return(
+    <div>
+        <h1 onClick={()=>{
+          obj.current *= 5
+          console.log('new object ref', obj)
+        }}>Hello world { count }</h1>
+    </div>
+  )
 }
 
 export default App;
